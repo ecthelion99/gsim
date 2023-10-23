eb53d5f9046bab0f029cdede8d66d265affacda1
ecthelion99 <tstr0006@student.monash.edu>
2023-10-23 23:57:06 +1100

    Added dependencies

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{pkgs ? import <nixpkgs> {
+    overlays = [(import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))];
+  }}:
+with pkgs;
+ mkShell {
+    nativeBuildInputs = [
+      pkg-config
+    ];
+    buildInputs = [
+      rust-bin.stable.latest.default
+      rust-analyzer
+      udev
+      alsa-lib
+      vulkan-loader
+      xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
+      libxkbcommon wayland
+    ];
+  }
